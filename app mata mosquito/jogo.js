678dac94bc1f71df8158ead41549d0d8e65a24da
Daniel Araujo <98916747+Daniel-dkf@users.noreply.github.com>
2023-07-28 01:35:10 -0300

    projeto app mata mosquito

--- app mata mosquito/jogo.js
+++ app mata mosquito/jogo.js
@@ -13,20 +13,60 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica(){
 
+    //remover o mosquito anterior (caso exista)
+
+   if( document.getElementById('mosquito')){
+    document.getElementById('mosquito').remove()
+   }
+
     let posicaoX = Math.floor(Math.random() * largura) - 90
     let posicaoY = Math.floor(Math.random() * altura) - 90 
     
     posicaoX = posicaoX < 0 ? 0 : posicaoX
-    posicaoX = posicaoY < 0 ? 0 : posicaoY
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
 
     console.log(posicaoX, posicaoY)
 
     //Criar o elemento html
     let mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosquito.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio() +  ' ' + ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+
     document.body.appendChild(mosquito)
+
+    
+    
+}
+
+function tamanhoAleatorio(){
+    let classe = Math.floor(Math.random() * 3)
+
+    switch(classe){
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
+    
+}
+
+function ladoAleatorio(){
+    let classe = Math.floor(Math.random() * 2)
+
+    switch(classe){
+        case 0:
+            return 'ladoA'
+
+        case 1:
+            return 'ladoB'
+    
+    } 
 }
